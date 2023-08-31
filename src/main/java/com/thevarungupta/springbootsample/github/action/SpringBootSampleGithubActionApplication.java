17ccdc1d0321958d0b0991589e032c60894bdc9e
Varun Gupta <thevarungupta5@gmail.com>
2023-08-31 11:09:57 +0530

    initial commit

---
+++ src/main/java/com/thevarungupta/springbootsample/github/action/SpringBootSampleGithubActionApplication.java
@@ -0,0 +1,13 @@
+package com.thevarungupta.springbootsample.github.action;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSampleGithubActionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSampleGithubActionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thevarungupta/springbootsample/github/action/SpringBootSampleGithubActionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thevarungupta.springbootsample.github.action;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSampleGithubActionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
